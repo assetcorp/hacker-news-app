b7b41f60297b2ca03e62e7f1cd7dc58f78b6d738
Delali <michaelazumah@live.com>
2021-09-21 19:53:07 +0000

    modify app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+import React from 'react'
+import '../src/app.css'
+// Components
+import App from 'next/app'
+import WebApp from '../src/components/App'
+import MainApp from '../src/components/MainApp'
+import Header from '../src/components/Header'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const NewsApp = ( { Component, pageProps, router } ) => {
+
+  const AppComponent = (
+    <WebApp>
+      <Header />
+      <MainApp>
+        <Component {...pageProps} />
+      </MainApp>
+    </WebApp>
+  )
+
+  // eslint-disable-next-line no-undef
+  return (
+    <div>
+      {AppComponent}
+    </div>
+  )
 }
 
-export default MyApp
+NewsApp.getInitialProps = async ( appContext ) => {
+  const appProps = await App.getInitialProps( appContext )
+  return { ...appProps }
+}
+
+export default NewsApp
